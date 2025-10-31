acba7bb585b2c8040d1c742b26f7fe72a67b1d9c
Javier Romero <javierromero1@fb.com>
2025-10-31 11:42:02 +0100

    added consistent copyright header

--- tools/smpl_mhr_conversion/fitting_mhr_with_pymomentum.py
+++ tools/smpl_mhr_conversion/fitting_mhr_with_pymomentum.py
@@ -1,3 +1,10 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 """PyMomentum-based fitting classes for MHR model conversion
 
 This module provides PyMomentum-based optimization tools for fitting MHR models
